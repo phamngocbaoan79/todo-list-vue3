d2b6416ce1e661adeba3669fbe7bd9f4026d4690
Phạm Ngọc Bảo An <phamngocbaoan.it@gmail.com>
2025-10-09 23:21:15 +0700

    course form

--- src/components/course/CourseForm.vue
+++ src/components/course/CourseForm.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>
-
+  <form @submit.prevent="save">
+    <input v-model="form.name" class="border p-2 w-full mb-3" placeholder="Tên khóa học" />
+    <div class="flex justify-end gap-2">
+      <button type="button" @click="$emit('close')">Huỷ</button>
+      <button type="submit">Lưu</button>
     </div>
+  </form>
 </template>
 
-<script>
-</script>
+<script setup>
+import { ref, watch } from 'vue'
+const props = defineProps({ data: Object })
+const emit = defineEmits(['close', 'saved'])
 
-<style>
-</style>
+const form = ref({ name: '' })
+
+watch(() => props.data, (val) => {
+  form.value = val ? { ...val } : { name: '' }
+}, { immediate: true })
+
+const save = async () => {
+  if (props.data) {
+    // update
+  } else {
+    // create
+  }
+  emit('saved')
+}
+</script>
